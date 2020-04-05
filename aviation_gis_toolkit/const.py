04d21522c6fbd59b1aa4fbd4773e47e97ce987f1
strpaw <38607111+strpaw@users.noreply.github.com>
2020-04-05 09:53:52 +0200

    Added coordinate_predetermined module with DMSH, HDMS compacted support
    Added support for DMSH, HDMS formats

--- aviation_gis_toolkit/const.py
+++ aviation_gis_toolkit/const.py
@@ -12,3 +12,13 @@ UOM_FT = 'ft'
 UOM_SM = 'SM'
 
 UOM_LIST = [UOM_M, UOM_KM, UOM_NM, UOM_FT, UOM_SM]
+
+# Angle types
+AT_LON = 'AT_LON'
+AT_LAT = 'AT_LAT'
+
+# Angle formats
+
+# Degrees, minutes, seconds, hemisphere - compacted formats:
+AF_DMSH_COMP = 'AF_DMSH_COMP'  # e.g.: 552243.47N
+AF_HDMS_COMP = 'AF_HDMS_COMP'  # e.g.: N552243.47
