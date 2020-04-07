4089beb04d3117832bc95ae7a5a09bcc81f13da5
strpaw <38607111+strpaw@users.noreply.github.com>
2020-04-07 07:41:13 +0200

    Added angle class with angle validation, conversion (DMS-DD, DD-DMS) formats - format not known in advance.

--- aviation_gis_toolkit/const.py
+++ aviation_gis_toolkit/const.py
@@ -3,7 +3,7 @@ const.py
 const module provides common constants as unit of measure, angle types used in aviation_gis_toolkit package.
 """
 
-# Units of measure
+# ------------------- Units of measure ------------------- #
 
 UOM_M = 'm'
 UOM_KM = 'km'
@@ -13,11 +13,13 @@ UOM_SM = 'SM'
 
 UOM_LIST = [UOM_M, UOM_KM, UOM_NM, UOM_FT, UOM_SM]
 
+# ------------------- Angle types ------------------- #
+
 # Angle types
 AT_LON = 'AT_LON'
 AT_LAT = 'AT_LAT'
 
-# Angle formats
+# ------------------- Angle formats ------------------- #
 
 # Degrees, minutes, seconds, hemisphere - compacted formats:
 AF_DMSH_COMP = 'AF_DMSH_COMP'  # e.g.: 552243.47N
@@ -30,3 +32,12 @@ AF_HDM_COMP = 'AF_HDM_COMP'  # e.g.: N5522.47
 # Degrees, hemisphere - compacted formats:
 AF_DH_COMP = 'AF_DH_COMP'  # e.g.: 55.47N
 AF_HD_COMP = 'AF_HD_COMP'  # e.g.: N55.47
+
+# 'Separated' formats
+AF_DMSH_ALL_SEP = 'AF_DMSH_ALL_SEP'  # e.g.: 55 22 43.47 N
+AF_HDMS_ALL_SEP = 'AF_HDMS_ALL_SEP'
+AF_DMSH_SEP = 'AF_DMSH_SEP'  # e.g.: 55 22 43.47N
+AF_HDMS_SEP = 'AF_HDMS_SEP'
+AF_DMSH_SEP_SYMBOLS = 'AF_DMSH_SEP_SYMBOLS'
+AF_HDMS_SEP_SYMBOLS = 'AF_HDMS_SEP_SYMBOLS'
+
