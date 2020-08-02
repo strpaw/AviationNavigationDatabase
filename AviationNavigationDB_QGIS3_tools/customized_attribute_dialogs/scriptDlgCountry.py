4ffcf601f2db3959eaa331e8a79ecb747dee9426
unknown <@>
2020-08-02 12:14:14 +0200

    Add GUI customied attribute dialog for coutry

---
+++ AviationNavigationDB_QGIS3_tools/customized_attribute_dialogs/scriptDlgCountry.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+QGIS forms can have a Python function that is called when the form is
+opened.
+
+Use this function to add extra logic to your forms.
+
+Enter the name of the function in the "Python Init function"
+field.
+An example follows:
+"""
+from qgis.PyQt.QtWidgets import QWidget
+
+def my_form_open(dialog, layer, feature):
+	geom = feature.geometry()
+	control = dialog.findChild(QWidget, "MyLineEdit")
